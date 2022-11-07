ba08a860df83cb0d5215a220752441bd6287bbfd
humayonitbd <humayonforid44@gmail.com>
2022-11-07 21:28:27 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+//middle ware
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res)=>{
+    res.send('server is running...')
+})
+
+
+
+
+
+app.listen(port,()=>{
+    console.log('your port is runnig', port)
+})
