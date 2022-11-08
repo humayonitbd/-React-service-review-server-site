cdd927694e3a730d39e163c81eb6caf222b4ae48
humayonitbd <humayonforid44@gmail.com>
2022-11-08 07:53:57 +0600

    single service get

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -17,7 +17,7 @@ app.get('/', (req, res)=>{
 
 
 const uri = `mongodb+srv://services-reviews:zrkNJWfGhFidpMGT@cluster0.epqkzkd.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
+// console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function dbConnect(){
@@ -34,6 +34,7 @@ async function dbConnect(){
 dbConnect();
 const servicesConnection = client.db('services-reviews').collection('services');
 
+//service data load home page
 app.get('/services', async(req, res)=>{
     try {
         const query = {};
@@ -48,6 +49,34 @@ app.get('/services', async(req, res)=>{
     }
 })
 
+//service all data load service page 
+app.get('/allService', async(req, res)=>{
+    try {
+        const query = {};
+        const cursor = servicesConnection.find(query);
+        const allServices = await cursor.toArray();
+        res.send(allServices)
+        
+    } catch (error) {
+        console.log(error.message)
+        
+    }
+})
+
+
+//single services data load 
+app.get('/allService/:id', async(req, res)=>{
+    try {
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)}
+        const singleService = await servicesConnection.findOne(query);
+        res.send(singleService)
+        
+    } catch (error) {
+        console.log(error.message)
+        
+    }
+})
 
 
 
